8425507a034c9e2075274bccd3f294059165d454
Chen <18551681083@163.com>
2022-10-18 21:59:47 +0800

    测试类初始化完成

---
+++ src/test/java/org/huhu/spring/security/demo/TestApp.java
@@ -0,0 +1,14 @@
+package org.huhu.spring.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApp {
+
+    @Test
+    void test() {
+        System.out.println("hello spring security");
+    }
+
+}
