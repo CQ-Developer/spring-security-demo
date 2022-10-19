84264b2c88b077a8c9deb8316a117c8b893c03ce
qchen71 <qchen71@ford.com>
2022-10-19 16:09:55 +0800

    自定义一个简单的UserDetailsService和PasswordEncoder

---
+++ src/main/java/org/huhu/spring/security/demo/configuration/ProjectConfig.java
@@ -0,0 +1,46 @@
+package org.huhu.spring.security.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+
+    /**
+     * <p>自定义一个 {@link UserDetailsService} 用于获取用户信息,
+     * 使用基于内存的实现 {@link InMemoryUserDetailsManager} 比较方便演示.
+     *
+     * <p>通过 {@link User} 创建一个用户并将其保存到 {@link UserDetailsService} 中
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        // 创建用户
+        UserDetails user = User.withUsername("john")
+                .password("123456")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    /**
+     * 为 {@link UserDetailsService} 配置一个密码编码器,
+     * 该 {@link NoOpPasswordEncoder} 不会对密码进行任何编码,
+     * 而只是简单的使用 {@link String#equals(Object)} 对密码进行验证.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
