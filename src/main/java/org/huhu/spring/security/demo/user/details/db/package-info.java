87135b558f8fd61c10b28c36e1dfcfb17a47a705
qchen71 <qchen71@ford.com>
2022-10-21 15:27:56 +0800

    添加package-info信息

---
+++ src/main/java/org/huhu/spring/security/demo/user/details/db/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 展示如何让自定义的用户适配SpringSecurity的UserDetails
+ *
+ * @see org.springframework.security.core.userdetails.UserDetails
+ */
+package org.huhu.spring.security.demo.user.details.db;
---
+++ src/main/java/org/huhu/spring/security/demo/user/details/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 展示如何自定义UserDetails
+ *
+ * @see org.springframework.security.core.userdetails.UserDetails
+ */
+package org.huhu.spring.security.demo.user.details;
