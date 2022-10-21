de63237b2814bbb67c6ae68c1176beec18179d29
qchen71 <qchen71@ford.com>
2022-10-21 17:59:13 +0800

    配置H2数据库以便后面测试JDBC

--- src/main/java/org/huhu/spring/security/demo/config/ProjectConfig.java
+++ src/main/java/org/huhu/spring/security/demo/config/ProjectConfig.java
@@ -5,10 +5,12 @@ import org.huhu.spring.security.demo.service.InMemoryUserDetailsService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 
@@ -33,6 +35,21 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    /**
+     * 该配置仅允许访问H2数据库控制台
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic()
+                .and()
+                .csrf().ignoringAntMatchers("/h2/**")
+                .and()
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .build();
     }
 
 }
---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO users VALUES (NULL, 'john', '127345', '1');
+INSERT IGNORE INTO authorities VALUES (NULL, 'john', 'write');
