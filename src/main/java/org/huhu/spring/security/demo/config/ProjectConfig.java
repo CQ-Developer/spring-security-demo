db3f0043ad2ecba92a03f7215bae1f7595ec42b7
Chen <18551681083@163.com>
2022-10-21 23:01:41 +0800

    通过ldif文件创建一个嵌入式LDAP服务器 并通过LdapUserDetailsManager访问用户

--- src/main/java/org/huhu/spring/security/demo/config/ProjectConfig.java
+++ src/main/java/org/huhu/spring/security/demo/config/ProjectConfig.java
@@ -1,43 +1,37 @@
 package org.huhu.spring.security.demo.config;
 
-import org.huhu.spring.security.demo.model.User;
-import org.huhu.spring.security.demo.service.InMemoryUserDetailsService;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
 
 /**
- * 测试自定义实现 {@link User} 和 {@link InMemoryUserDetailsService}
+ * 一个简单的 LDAP 实现的 {@link UserDetailsManager}
  *
- * 将 {@link InMemoryUserDetailsService} 注册到 {@link ApplicationContext} 中,
- * 并提供一个 {@link PasswordEncoder}
- *
- * @see User
- * @see InMemoryUserDetailsService
+ * @see LdapUserDetailsManager
  */
 @Configuration
 public class ProjectConfig {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
-        // 使用数据库访问用户信息
-        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // 创建上下文源指定LDAP服务器地址
+        DefaultSpringSecurityContextSource contextSource = new DefaultSpringSecurityContextSource("ldap://localhost:33389/dc=springframework,dc=org");
+        contextSource.afterPropertiesSet();
 
-        // 自定义查询
-        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-
-        // 自定义查询
-        String authoritiesByUsernameQuery = "select username, authority from authorities where username = ?";
-        userDetailsManager.setAuthoritiesByUsernameQuery(authoritiesByUsernameQuery);
+        // 创建LdapUserDetailsManager的实例
+        LdapUserDetailsManager userDetailsManager = new LdapUserDetailsManager(contextSource);
+        // 设置用户名映射器指定LdapUserDetailsManager如何搜索用户
+        userDetailsManager.setUsernameMapper(new DefaultLdapUsernameToDnMapper("ou=groups", "uid"));
+        // 设置应用程序搜索用户所需的群组搜索库
+        userDetailsManager.setGroupSearchBase("ou=groups");
 
         return userDetailsManager;
     }
@@ -45,21 +39,6 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }
-
-    /**
-     * 该配置仅允许访问H2数据库控制台
-     */
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .httpBasic()
-                .and()
-                .csrf().ignoringAntMatchers("/h2/**")
-                .and()
-                .headers().frameOptions().sameOrigin()
-                .and()
-                .build();
     }
 
 }
