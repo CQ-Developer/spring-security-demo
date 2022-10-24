d55c9388c0d7253bb2fca0cf34479e4ec19e96a4
qchen71 <qchen71@ford.com>
2022-10-24 16:24:35 +0800

    通过SecurityContextHolder在controller层访问当前已认证的用户

--- src/main/java/org/huhu/spring/security/demo/controller/HelloController.java
+++ src/main/java/org/huhu/spring/security/demo/controller/HelloController.java
@@ -1,5 +1,8 @@
 package org.huhu.spring.security.demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,10 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello!";
+        // 通过SecurityContextHolder访问当前已认证的用户
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        return "Hello, " + authentication.getName() + "!";
     }
 
 }
