6710bf2faf1a0a350d8f3bda30c9767094aefc93
qchen71 <qchen71@ford.com>
2022-10-25 16:07:17 +0800

    通过FormLoginConfigurer进一步配置FormLogin登录

--- src/main/java/org/huhu/spring/security/demo/config/ProjectConfig.java
+++ src/main/java/org/huhu/spring/security/demo/config/ProjectConfig.java
@@ -3,21 +3,34 @@ package org.huhu.spring.security.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
+    /**
+     * 配置formLogin登录方式.
+     *
+     * 当用户未登录时, 将被转发到/login
+     * 访问/logout将登出
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .formLogin()
-                .and()
+                .formLogin(this::customizeFormLogin)
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .build();
+    }
+
+    /**
+     * 使用 {@link FormLoginConfigurer} 进一步配置FormLogin登录方式
+     */
+    private void customizeFormLogin(FormLoginConfigurer<HttpSecurity> formLoginConfigurer) {
+        formLoginConfigurer.defaultSuccessUrl("/home", true);
     }
 
 }
