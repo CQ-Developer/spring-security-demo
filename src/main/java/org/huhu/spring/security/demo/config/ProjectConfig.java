c83b93b5060bf13e4d79e66a4419b93337e9e50c
qchen71 <qchen71@ford.com>
2022-10-26 16:34:35 +0800

    通过"ROLE_"前缀为用户添加角色 通过hasRole()方法限定只有具备指定角色的用户的才能访问

--- src/main/java/org/huhu/spring/security/demo/config/ProjectConfig.java
+++ src/main/java/org/huhu/spring/security/demo/config/ProjectConfig.java
@@ -14,31 +14,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ProjectConfig {
 
-    private static final String AUTHORITY_READ = "READ";
-
-    private static final String AUTHORITY_WRITE = "WRITE";
-
-    private static final String AUTHORITY_DELETE = "DELETE";
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.httpBasic();
-        // 只有具备READ权限且不具备DELETE权限的用户才能访问
+        // 只有ADMIN角色才能访问
         httpSecurity.authorizeRequests()
                     .anyRequest()
-                    .access("hasAuthority('READ') and !hasAuthority('DELETE')");
+                    .hasRole("ADMIN");
         return httpSecurity.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
+        // ROLE_ADMIN: read, write, update, delete
         UserDetails john = User.withUsername("john")
                                .password("123")
-                               .authorities(AUTHORITY_READ)
+                               .authorities("ROLE_ADMIN")
                                .build();
+        // ROLE_MANAGER: read, write
         UserDetails jane = User.withUsername("jane")
                                .password("123")
-                               .authorities(AUTHORITY_READ, AUTHORITY_WRITE, AUTHORITY_DELETE)
+                               .authorities("ROLE_MANAGER")
                                .build();
         return new InMemoryUserDetailsManager(john, jane);
     }
