56471d4c569aaca9b8dc519628c6b0d515f5df1e
qchen71 <qchen71@ford.com>
2022-10-26 17:58:59 +0800

    通过mvcMatchers(HTTPMethod, String...)配置 允许所有已认证的用户通过GET方法访问"/a"端点 允许所有人()通过POST方法访问"/a"端点 拒绝任何人对其他端点访问

---
+++ src/main/java/org/huhu/spring/security/demo/conf/AppSecurityConfiguration.java
@@ -0,0 +1,51 @@
+package org.huhu.spring.security.demo.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+public class AppSecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails john = User.withUsername("john")
+                               .password("123")
+                               .roles("ADMIN")
+                               .build();
+        UserDetails jane = User.withUsername("Jane")
+                               .password("123")
+                               .roles("MANAGER")
+                               .build();
+        return new InMemoryUserDetailsManager(john, jane);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic();
+        // 禁用CSRF已方便演示
+        httpSecurity.csrf()
+                    .disable();
+        httpSecurity.authorizeRequests()
+                    .mvcMatchers(GET, "/a").authenticated()
+                    .mvcMatchers(POST, "/a").permitAll()
+                    .anyRequest().denyAll();
+        return httpSecurity.build();
+    }
+
+}
