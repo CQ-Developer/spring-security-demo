33da11a69c600561f3656205525a6b6e355b400e
qchen71 <qchen71@ford.com>
2022-10-28 13:35:34 +0800

    通过继承Spring提供的OncePerRequestFilter, 保证自定义的过滤器在每个请求中只会执行一次.

--- src/main/java/org/huhu/spring/security/demo/conf/security/filter/CustomizedAuthenticationLoggingFilter.java
+++ src/main/java/org/huhu/spring/security/demo/conf/security/filter/CustomizedAuthenticationLoggingFilter.java
@@ -2,29 +2,32 @@ package org.huhu.spring.security.demo.conf.security.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * 自定义个 {@link Filter} 的实现,
  * 在成功认证之后记录 {@code Request-Id} 信息.
+ *
+ * 通过 {@link OncePerRequestFilter} 保证该过滤器每个请求只会执行一次
+ *
+ * @see OncePerRequestFilter
  */
-public class CustomizedAuthenticationLoggingFilter implements Filter {
+public class CustomizedAuthenticationLoggingFilter extends OncePerRequestFilter {
 
     private final Logger logger = LoggerFactory.getLogger(CustomizedAuthenticationLoggingFilter.class);
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String requestId = httpRequest.getHeader("Request-Id");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestId = request.getHeader("Request-Id");
         logger.info("Successfully authenticated request with id {}", requestId);
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 
 }
