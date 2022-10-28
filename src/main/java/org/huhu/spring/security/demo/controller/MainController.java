648fb0ea7af96b9853742edebae8e02b61717f40
Chen <18551681083@163.com>
2022-10-28 23:22:53 +0800

    通过@CrossOrigin直接直接为指定的端点进行CORS配置

--- src/main/java/org/huhu/spring/security/demo/controller/MainController.java
+++ src/main/java/org/huhu/spring/security/demo/controller/MainController.java
@@ -3,6 +3,7 @@ package org.huhu.spring.security.demo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +20,7 @@ public class MainController {
 
     @ResponseBody
     @PostMapping("/test")
+    @CrossOrigin(origins = {"http://localhost:8080"})
     public String test() {
         logger.info("Test method called");
         return "HELLO";
