f92b871ed39222829ad811c1fac8633f42eb16c3
qchen71 <qchen71@ford.com>
2022-10-28 13:21:20 +0800

    通过排除UserDetailsServiceAutoConfiguration自动装配类 取消SpringBoot自动装配的用户

--- src/main/java/org/huhu/spring/security/demo/App.java
+++ src/main/java/org/huhu/spring/security/demo/App.java
@@ -2,12 +2,13 @@ package org.huhu.spring.security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 import java.util.TimeZone;
 
 import static java.time.ZoneOffset.UTC;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class App {
 
     public static void main(String[] args) {
