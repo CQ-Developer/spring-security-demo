14e9133ef483675f29488be3f15d435a7d6e5ae6
Chen <18551681083@163.com>
2022-10-29 10:52:18 +0800

    通过CorsConfigurer自定义CORS源

--- src/main/java/org/huhu/spring/security/demo/config/WebSecurityConfiguration.java
+++ src/main/java/org/huhu/spring/security/demo/config/WebSecurityConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Configuration
 public class WebSecurityConfiguration {
@@ -17,7 +21,25 @@ public class WebSecurityConfiguration {
         httpSecurity.authorizeRequests()
                     .anyRequest()
                     .permitAll();
+        // CORS配置
+        httpSecurity.cors()
+                    .configurationSource(this::customizeCorsConfiguration);
         return httpSecurity.build();
+    }
+
+    /**
+     * 通过 {@link org.springframework.security.config.annotation.web.configurers.CorsConfigurer}
+     * 配置 {@link org.springframework.web.cors.CorsConfigurationSource} 对象,
+     * 实现自定义 CORS 配置.
+     *
+     * @see org.springframework.web.cors.CorsConfigurationSource
+     * @see org.springframework.web.cors.CorsConfiguration
+     */
+    private CorsConfiguration customizeCorsConfiguration(HttpServletRequest request) {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8080"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        return corsConfiguration;
     }
 
 }
--- src/main/java/org/huhu/spring/security/demo/controller/MainController.java
+++ src/main/java/org/huhu/spring/security/demo/controller/MainController.java
@@ -3,7 +3,6 @@ package org.huhu.spring.security.demo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +19,6 @@ public class MainController {
 
     @ResponseBody
     @PostMapping("/test")
-    @CrossOrigin(origins = {"http://localhost:8080"})
     public String test() {
         logger.info("Test method called");
         return "HELLO";
