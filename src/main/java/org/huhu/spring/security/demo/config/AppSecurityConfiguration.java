b18945db2e31896b6e9f8d2b53ae9d31ec1403ae
qchen71 <qchen71@ford.com>
2022-11-01 13:09:26 +0800

    定义ClientRegistrationRepository对象: 第一种方式, 通过将ClientRegistrationRepository作为bean注册到Spring上下文中

--- src/main/java/org/huhu/spring/security/demo/config/AppSecurityConfiguration.java
+++ src/main/java/org/huhu/spring/security/demo/config/AppSecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.oauth2.client.CommonOAuth2Provider.GITHUB;
@@ -18,6 +20,11 @@ public class AppSecurityConfiguration {
                     .anyRequest()
                     .authenticated();
         return httpSecurity.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(clientRegistration());
     }
 
     private ClientRegistration clientRegistration() {
