6a3e1a9c827fca7d9bdd0fdb3b7a822aa46959c2
qchen71 <qchen71@ford.com>
2022-11-01 13:40:37 +0800

    可以通过SecurityContext或直接注入的方式获得OAuth2AuthenticationToken

--- src/main/java/org/huhu/spring/security/demo/controller/MainController.java
+++ src/main/java/org/huhu/spring/security/demo/controller/MainController.java
@@ -1,13 +1,19 @@
 package org.huhu.spring.security.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    private final Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @GetMapping("/")
-    public String main() {
+    public String main(OAuth2AuthenticationToken authentication) {
+        logger.info("obtain OAuth2User from OAuth2AuthenticationToken: {}", authentication.getPrincipal());
         return "main.html";
     }
 
