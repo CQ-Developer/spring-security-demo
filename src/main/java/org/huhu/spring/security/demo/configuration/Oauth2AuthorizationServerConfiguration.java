0952a36417cb08608245eff482a61d079d9a8701
Chen <18551681083@163.com>
2022-11-01 22:56:28 +0800

    配置ClientDetails和ClientDetailsService

--- src/main/java/org/huhu/spring/security/demo/configuration/Oauth2AuthorizationServerConfiguration.java
+++ src/main/java/org/huhu/spring/security/demo/configuration/Oauth2AuthorizationServerConfiguration.java
@@ -2,9 +2,16 @@ package org.huhu.spring.security.demo.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.InMemoryClientDetailsService;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 通过注解 {@link EnableAuthorizationServer} 指示SpringBoot启用OAuth2授权服务器的配置.
@@ -25,9 +32,29 @@ public class Oauth2AuthorizationServerConfiguration extends AuthorizationServerC
         this.authenticationManager = authenticationManager;
     }
 
+    /**
+     * OAuth2授权服务器通过 {@link AuthenticationManager} 管理用户.
+     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
+    }
+
+    /**
+     * OAuth2授权服务器通过 {@link ClientDetailsService} 管理客户端.
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        BaseClientDetails clientDetails = new BaseClientDetails();
+        clientDetails.setClientId("client");
+        clientDetails.setClientSecret("secret");
+        clientDetails.setScope(List.of("read"));
+        clientDetails.setAuthorizedGrantTypes(List.of("password"));
+
+        InMemoryClientDetailsService clientDetailsService = new InMemoryClientDetailsService();
+        clientDetailsService.setClientDetailsStore(Map.of("client", clientDetails));
+
+        clients.withClientDetails(clientDetailsService);
     }
 
 }
