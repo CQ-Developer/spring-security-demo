b6ffbb8b367e88d3b723cdd71b96e96a2118b6ff
qchen71 <qchen71@ford.com>
2022-11-01 10:57:28 +0800

    修改配置类: 通过SpringSecurity提供的CommonOAuth2Provider进行配置

--- src/main/java/org/huhu/spring/security/demo/config/AppSecurityConfiguration.java
+++ src/main/java/org/huhu/spring/security/demo/config/AppSecurityConfiguration.java
@@ -4,10 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.oauth2.core.AuthorizationGrantType.AUTHORIZATION_CODE;
+import static org.springframework.security.config.oauth2.client.CommonOAuth2Provider.GITHUB;
 
 @Configuration
 public class AppSecurityConfiguration {
@@ -22,18 +21,10 @@ public class AppSecurityConfiguration {
     }
 
     private ClientRegistration clientRegistration() {
-        return ClientRegistration.withRegistrationId("github")
-                                 .clientId("147b7388f3f222a8a6cc")
-                                 .clientSecret("e74c289ab246d6dac1fc00c5790771d466ac3064")
-                                 .scope("read:user")
-                                 .authorizationUri("https://github.com/login/oauth/authorize")
-                                 .tokenUri("https://github.com/login/oauth/access_token")
-                                 .userInfoUri("https://api.github.com/user")
-                                 .userNameAttributeName("id")
-                                 .clientName("GitHub")
-                                 .authorizationGrantType(AUTHORIZATION_CODE)
-                                 .redirectUri("{baseUrl}/{action}/oauth2/code/{registrationId")
-                                 .build();
+        return GITHUB.getBuilder("github")
+                     .clientId("147b7388f3f222a8a6cc")
+                     .clientSecret("e74c289ab246d6dac1fc00c5790771d466ac3064")
+                     .build();
     }
 
 }
