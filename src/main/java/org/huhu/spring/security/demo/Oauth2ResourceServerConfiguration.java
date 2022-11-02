4ce11f91b5bd3b1f9de043e8645575c2c266fb6e
Chen <18551681083@163.com>
2022-11-02 22:42:45 +0800

    配置资源服务器

---
+++ src/main/java/org/huhu/spring/security/demo/Oauth2ResourceServerConfiguration.java
@@ -0,0 +1,9 @@
+package org.huhu.spring.security.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+public class Oauth2ResourceServerConfiguration {
+}
