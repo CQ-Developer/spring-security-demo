2df479e0eb1d51f9db987b365086cdca77ef8643
qchen71 <qchen71@ford.com>
2022-11-04 14:43:48 +0800

    通过配置公私钥的方式完成非堆成加密

--- resource-server/src/main/java/org/huhu/jwt/config/ResourceServerWebSecurityConfigurer.java
+++ resource-server/src/main/java/org/huhu/jwt/config/ResourceServerWebSecurityConfigurer.java
@@ -7,28 +7,53 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 @Configuration
 public class ResourceServerWebSecurityConfigurer {
 
-    private final String jwtKey = "ymLTU8rq83j4fmJZj60wh4OrMNuntIj4fmJ";
+    private final String jwtPubKey =
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApSVcWYnS9NusbIs0/H0B" +
+            "UjNLTsiIdeg/aa5nFr86/BxrxW214YWYuHyuTM5XHMD9LAxbRLgiQJG13xm1uZfY" +
+            "Xf+M/VGZRCnPEVdir0N7YFbk1CdWcwUfGZrfogx90kNncaUEk6lshCkwPqBavfPT" +
+            "5BkzPVj8XtRmz6hkmXceMy8l7qvCGTl7+wHJtjkJCURsOX3uY67TV3lg/OZ6smZa" +
+            "Qc6Xl8pVnHqqx9bzi/wU+ApOqHZByRNWBR+V5NIEskxKP0fVPfjGPqokmCkbD3Ef" +
+            "/8H/4RfQZ7zSxzNf49A2alFrCq2F0kC0TQVAr3SdSPX8fFzHTVFXXdbGxLEoUu+7" +
+            "MwIDAQAB";
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
                     .anyRequest().authenticated();
 
-        SecretKey secretKey = new SecretKeySpec(jwtKey.getBytes(UTF_8), "AES");
-        JwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(secretKey).build();
+        RSAPublicKey rsaPublicKey = loadRSAPublicKey();
+        JwtDecoder jwtDecoder = NimbusJwtDecoder.withPublicKey(rsaPublicKey).build();
+
         httpSecurity.oauth2ResourceServer()
                     .jwt()
                     .decoder(jwtDecoder);
 
         return httpSecurity.build();
+    }
+
+    private RSAPublicKey loadRSAPublicKey() {
+        try {
+            String alg = "RSA";
+            KeyFactory keyFactory = KeyFactory.getInstance(alg);
+
+            Base64.Decoder decoder = Base64.getDecoder();
+            KeySpec keySpec = new X509EncodedKeySpec(decoder.decode(jwtPubKey), alg);
+
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
