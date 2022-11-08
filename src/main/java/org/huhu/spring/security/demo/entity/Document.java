4b74c00f78debe14ea373fde20436b62ebd2ef73
qchen71 <qchen71@ford.com>
2022-11-08 10:15:28 +0800

    创建Document实体类

---
+++ src/main/java/org/huhu/spring/security/demo/entity/Document.java
@@ -0,0 +1,19 @@
+package org.huhu.spring.security.demo.entity;
+
+public class Document {
+
+    private String owner;
+
+    public Document(String owner) {
+        this.owner = owner;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+}
