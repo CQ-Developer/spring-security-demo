828023a953be7f06ec31d9244bfe2ceb1433bffc
qchen71 <qchen71@ford.com>
2022-11-09 10:52:39 +0800

    通过@PostFilter注解定义后过滤规则

--- src/main/java/org/huhu/spring/security/demo/controller/ProductController.java
+++ src/main/java/org/huhu/spring/security/demo/controller/ProductController.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedList;
 import java.util.List;
 
 @RestController
@@ -21,20 +20,9 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/sell")
-    public List<Product> sell() {
-        // 模拟客户端的传参
-        List<Product> products = new LinkedList<>();
-        products.add(new Product("beer", "jack"));
-        products.add(new Product("candy", "jack"));
-        products.add(new Product("chocolate", "rose"));
-
-        // 注意@PreFilter会直接修改参数的值
-        logger.info("before @PreFilter, list size is {}", products.size());
-        List<Product> result = productService.sellProducts(products);
-        logger.info("after @PreFilter, list size is {}", products.size());
-
-        return result;
+    @GetMapping("/find")
+    public List<Product> findProducts() {
+        return productService.sellProducts();
     }
 
 }
--- src/main/java/org/huhu/spring/security/demo/service/ProductService.java
+++ src/main/java/org/huhu/spring/security/demo/service/ProductService.java
@@ -2,33 +2,44 @@ package org.huhu.spring.security.demo.service;
 
 import org.huhu.spring.security.demo.config.AppSecurityConfiguration;
 import org.huhu.spring.security.demo.entity.Product;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class ProductService {
 
     /**
-     * <p>与 {@link PreAuthorize} 相同,
-     * 预过滤需要通过 {@link EnableGlobalMethodSecurity#prePostEnabled} 开启.
+     * <p>与 {@link PostAuthorize} 相同,
+     * 后过滤需要通过 {@link EnableGlobalMethodSecurity#prePostEnabled} 开启.
      *
-     * <p>通过在方法上定义 {@link PreFilter} 注解实现预过滤.
+     * <p>通过在方法上定义 {@link PostFilter} 注解实现后过滤.
      *
-     * <p>在 {@link PreFilter} 中通过 {@code filterObject} 可以引用集合参数中的元素.
+     * <p>在 {@link PostFilter} 中通过 {@code filterObject} 可以引用集合返回值中的元素.
      *
-     * <p>在 {@link PreFilter} 中通过 {@code authentication} 可以引用存储在 {@link SecurityContext}
+     * <p>在 {@link PostFilter} 中通过 {@code authentication} 可以引用存储在 {@link SecurityContext}
      * 中的 {@link Authentication} 对象.
+     *
+     * <p>同 {@link PreFilter} 只适用于集合或数组类型的参数,
+     * {@link PostFilter} 只适用于集合或数组类型的返回值.
      *
      * @see AppSecurityConfiguration
      */
-    @PreFilter("filterObject.owner == authentication.name")
-    public List<Product> sellProducts(List<Product> products) {
+    @PostFilter("filterObject.owner == authentication.name")
+    public List<Product> sellProducts() {
+        // 模拟数据库查询结果
+        List<Product> products = new LinkedList<>();
+        products.add(new Product("beer", "jack"));
+        products.add(new Product("candy", "jack"));
+        products.add(new Product("chocolate", "rose"));
+
         return products;
     }
 
