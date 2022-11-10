ebfb047f4290a18ed721032dced001eadac34b54
qchen71 <qchen71@ford.com>
2022-11-10 14:23:05 +0800

    完成接入层定义

---
+++ src/main/java/org/huhu/spring/security/demo/controller/WorkoutController.java
@@ -0,0 +1,38 @@
+package org.huhu.spring.security.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.huhu.spring.security.demo.entity.Workout;
+import org.huhu.spring.security.demo.service.WorkoutService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/workout")
+@RequiredArgsConstructor
+public class WorkoutController {
+
+    private final WorkoutService workoutService;
+
+    @PostMapping("/")
+    public void add(@RequestBody Workout workout) {
+        workoutService.saveWorkout(workout);
+    }
+
+    @GetMapping("/")
+    public List<Workout> findAll() {
+        return workoutService.findWorkouts();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        workoutService.deleteWorkout(id);
+    }
+
+}
