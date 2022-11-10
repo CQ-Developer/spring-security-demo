98f7e8efaad8bf6878c8a5124872ea7288f4e7bc
qchen71 <qchen71@ford.com>
2022-11-10 15:56:29 +0800

    在反应式环境中 通过Mono<Authentication>直接在接入层注入认证信息

--- src/main/java/org/huhu/spring/security/demo/controller/HelloController.java
+++ src/main/java/org/huhu/spring/security/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.huhu.spring.security.demo.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -8,8 +9,8 @@ import reactor.core.publisher.Mono;
 public class HelloController {
 
     @GetMapping("/hello")
-    public Mono<String> hello() {
-        return Mono.just("Hello!");
+    public Mono<String> hello(Mono<Authentication> authentication) {
+        return authentication.map(a -> "Hello, " + a.getName());
     }
 
 }
