44558d154d04082b214088b3aec6b76c1832977a
qchen71 <qchen71@ford.com>
2022-11-10 15:47:57 +0800

    在反应式环境中 通过ReactiveUserDetailsService管理用户

---
+++ src/main/java/org/huhu/spring/security/demo/config/AppSecurityConfig.java
@@ -0,0 +1,26 @@
+package org.huhu.spring.security.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public ReactiveUserDetailsService reactiveUserDetailsService() {
+        UserDetails jack = User.withUsername("jack").password("123456").authorities("read").build();
+        return new MapReactiveUserDetailsService(jack);
+    }
+
+}
