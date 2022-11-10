d25630f5bc4f39cccc0780a988b48439a7a50115
qchen71 <qchen71@ford.com>
2022-11-10 15:27:37 +0800

    一个简单的反应式应用程序

--- src/main/java/org/huhu/spring/security/demo/controller/HelloController.java
+++ src/main/java/org/huhu/spring/security/demo/controller/HelloController.java
@@ -2,13 +2,14 @@ package org.huhu.spring.security.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    public Mono<String> hello() {
+        return Mono.just("Hello!");
     }
 
 }
