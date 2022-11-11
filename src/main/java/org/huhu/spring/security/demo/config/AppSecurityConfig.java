0d9e8528de807732a31176eea9ab2ca4fcd56013
qchen71 <qchen71@ford.com>
2022-11-11 12:25:19 +0800

    在反应式项目中通过ServerHttpSecurity配置资源服务器 认证JWT令牌

--- src/main/java/org/huhu/spring/security/demo/config/AppSecurityConfig.java
+++ src/main/java/org/huhu/spring/security/demo/config/AppSecurityConfig.java
@@ -1,81 +1,33 @@
 package org.huhu.spring.security.demo.config;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec;
+import org.springframework.security.config.web.server.ServerHttpSecurity.OAuth2ResourceServerSpec;
+import org.springframework.security.config.web.server.ServerHttpSecurity.OAuth2ResourceServerSpec.JwtSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 
 @Configuration
-@EnableReactiveMethodSecurity
 public class AppSecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    /**
-     * 类比 {@link UserDetailsService}.
-     *
-     * <p>在 {@code Servlet} 环境中,
-     * {@link AuthenticationManager} 将认证委派给 {@link AuthenticationProvider},
-     * 再通过 {@link UserDetailsService} 找到指定用户.
-     *
-     * <p>在 {@code Reactive} 环境中,
-     * {@link AuthenticationWebFilter} 将认证委派给 {@link ReactiveAuthenticationManager}
-     * 并直接通过 {@link ReactiveUserDetailsService} 找到用户.
-     *
-     * @see AuthenticationWebFilter
-     * @see ReactiveAuthenticationManager
-     */
-    @Bean
-    public ReactiveUserDetailsService reactiveUserDetailsService() {
-        UserDetails jack = User.withUsername("jack").password("123456").roles("ADMIN").build();
-        UserDetails rose = User.withUsername("rose").password("123456").roles("USER").build();
-        return new MapReactiveUserDetailsService(jack, rose);
-    }
-
-    /**
-     * 配置授权规则.
-     *
-     * <p>在 {@code Servlet} 环境中可以通过 {@link HttpSecurity} 配置授权规则.
-     * 并通过将 {@link SecurityFilterChain} 作为 {@link Bean}
-     * 发布到 {@link ApplicationContext} 中, 从而应用授权规则.
-     *
-     * <p>在 {@code Reactive} 环境中则是通过 {@link ServerHttpSecurity} 配置授权规则.
-     * 并通过将 {@link SecurityWebFilterChain} 作为 {@link Bean}
-     * 发布到 {@link ApplicationContext} 中, 从而应用授权规则.
-     *
-     * @see ServerHttpSecurity
-     * @see SecurityWebFilterChain
-     * @see HttpSecurity
-     * @see SecurityFilterChain
-     */
-    @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
         return httpSecurity
-                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(this::doConfigureOath2ResourceService)
                 .authorizeExchange(this::doAuthorizeExchange)
                 .build();
     }
 
-    private void doAuthorizeExchange(ServerHttpSecurity.AuthorizeExchangeSpec exchangeMatcherRegistry) {
+    private void doConfigureOath2ResourceService(OAuth2ResourceServerSpec oAuth2ResourceServerSpec) {
+        oAuth2ResourceServerSpec.jwt(this::doConfigureJwt);
+    }
+
+    private void doConfigureJwt(JwtSpec jwtSpec) {
+        jwtSpec.jwkSetUri("http://localhost:8080/realms/master/protocol/openid-connect/certs");
+    }
+
+    private void doAuthorizeExchange(AuthorizeExchangeSpec exchangeMatcherRegistry) {
         exchangeMatcherRegistry.anyExchange().authenticated();
     }
 
